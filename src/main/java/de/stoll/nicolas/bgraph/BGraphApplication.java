14b46e48dd334c83cdbf70d6b62ed766b16e006b
nicolas <nicolasstoll@tutamail.com>
2026-02-04 18:53:20 +0100

    First commit

---
+++ src/main/java/de/stoll/nicolas/bgraph/BGraphApplication.java
@@ -0,0 +1,13 @@
+package de.stoll.nicolas.bgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BGraphApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BGraphApplication.class, args);
+    }
+
+}
